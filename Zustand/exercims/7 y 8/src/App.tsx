220981e6225d9c3aa90de86b6b5128a4bfdc9f33
kevinnncr <kacriollo@sudamericano.edu.ec>
2024-05-12 21:36:24 -0500

    zustand

---
+++ Zustand/exercims/7 y 8/src/App.tsx
@@ -0,0 +1,14 @@
+import CharacterGenerator from "./components/D&D"
+import AgeCalculator from "./components/spaceAge"
+
+
+export const App = () => {
+  return (
+    <>
+    <AgeCalculator/>
+    <CharacterGenerator/>
+    </>
+  )
+}
+
+export default App
